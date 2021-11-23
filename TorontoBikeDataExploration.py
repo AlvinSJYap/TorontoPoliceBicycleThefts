c3208fb1bb67d4fee25f837be63ef6366b515cb2
Razei <razeixyz@gmail.com>
2021-11-23 16:39:39 -0500

    Bit of console formatting to view outputs

--- TorontoBikeDataExploration.py
+++ TorontoBikeDataExploration.py
@@ -5,40 +5,62 @@ Created on Sun Nov 21 00:59:29 2021
 @author: asjya
 """
 
-import pandas as pd 
-import math 
-
+import pandas as pd
+import matplotlib.pyplot as plt
+import math
 import os
+
+def printSeparator(value):
+  print('\n' * 4)
+  print(value)
+  print('\n')
+
+plt.close("all")
 path = ".\data"
 filename = 'Bicycle_Thefts.csv'
 fullpath = os.path.join(path,filename)
 bike_df = pd.read_csv(fullpath)
+
+printSeparator(' RAW CSV ')
 print(bike_df)
+
 pd.set_option('display.max_columns',15)
+
+printSeparator('bike_df.head():')
 print(bike_df.head())
+
+printSeparator('bike_df.columns.values:')
 print(bike_df.columns.values)
+
+printSeparator('bike_df.shape:')
 print(bike_df.shape)
+
+printSeparator('bike_df.describe():')
 print(bike_df.describe())
-print(bike_df.dtypes) 
+
+printSeparator('bike_df.dtypes:')
+print(bike_df.dtypes)
+
+printSeparator('bike_df.head(5):')
 print(bike_df.head(5))
 
-print("# of Null Values: \n")
+printSeparator('# of Null Values:')
 print(bike_df.isnull().sum())
 
 
-print('\n Mean of Numerical Columns: \n')
+printSeparator('Mean of Numerical Columns:')
 print(bike_df.mean())
 '''
-There are 35 columns and 25569 rows in the bike dataframe. 
+There are 35 columns and 25569 rows in the bike dataframe.
 2 Columns should be datetime but are coming out as string. We can clean this up
 Bike_Make, Bike_Model, Bike_Colour, and Cost_of_Bike have numerous null values that we need to handle.
-''' 
+'''
 
-print('\n Mode of Categorical Columns: \n')
+printSeparator('Mode of Categorical Columns:')
 print(bike_df[bike_df.select_dtypes(include=['object']).columns.tolist()].mode())
 
 '''
-Convert the Obejct Data type of the Occurence Date and Report Date into a proper date because 
+Convert the Obejct Data type of the Occurence Date and Report Date into a proper date because
 Date Time is easier to work with.
 
 '''
@@ -49,7 +71,7 @@ bike_df['Occurrence_Date'] = bike_df['Occurrence_Date'].astype('datetime64')
 bike_df['Report_Date'] = pd.to_datetime(bike_df['Report_Date'])
 bike_df['Report_Date'] = bike_df['Report_Date'].dt.date
 bike_df['Report_Date'] = bike_df['Report_Date'].astype('datetime64')
-print(bike_df.dtypes) 
+print(bike_df.dtypes)
 '''
 Now we deal with the columns with missing values.
 
@@ -67,12 +89,16 @@ bike_df['Bike_Colour'] = bike_df['Bike_Colour'].fillna('Unknown Colour')
 
 bike_df['Cost_of_Bike'] = bike_df['Cost_of_Bike'].fillna(bike_df['Cost_of_Bike'].mean())
 
-print("# of Null Values: \n")
+printSeparator("# of Null Values:")
 print(bike_df.isnull().sum())
 
 '''
-The initial Data is cleaned up, we can save a copy of this as step 1. In step 2, we determine if there are any outliers 
+The initial Data is cleaned up, we can save a copy of this as step 1. In step 2, we determine if there are any outliers
 in the data set and can be removed.
 '''
 #bike_df.to_csv('.\data\Bicycle_Thefts_CleanStep1.csv')
 
+# plt.figure()
+
+# bike_df.plot()
+# plt.show()
