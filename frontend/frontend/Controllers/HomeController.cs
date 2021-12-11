d4938bda5b473456e856c2c639e7c396648a9c9f
Razei <razeixyz@gmail.com>
2021-12-10 20:15:42 -0500

    Make form and methods

--- frontend/frontend/Controllers/HomeController.cs
+++ frontend/frontend/Controllers/HomeController.cs
@@ -1,37 +1,44 @@
 ﻿using frontend.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace frontend.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
+
         }
 
         public IActionResult Index()
         {
-            return View();
+            return View(new BikeTheftViewModel());
         }
 
-        public IActionResult Privacy()
+        [HttpPost]
+        public async void Index(BikeTheftViewModel bikeTheftViewModel)
         {
-            return View();
-        }
+            using (var client = new HttpClient())
+            {
+                var json = JsonConvert.SerializeObject(bikeTheftViewModel);
+                var data = new StringContent(json, Encoding.UTF8, "application/json");
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+                var url = "https://comp309-group2-flaskapi.azurewebsites.net/predict";
+
+                var response = await client.PostAsync(url, data);
+
+                string result = response.Content.ReadAsStringAsync().Result;
+            }
         }
     }
 }
---
+++ frontend/frontend/Models/BikeTheftViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace frontend.Models
+{
+    public class BikeTheftViewModel
+    {
+        public string Bike_Model { get; set; }
+        public int Bike_Cost { get; set; }
+        public string Bike_Colour { get; set; }
+        public string Occurence_Date { get; set; }
+        public string Primary_Offence { get; set; }
+        public string Neighbourhood_Name { get; set; }
+        public string Location_Type { get; set; }
+    }
+}
